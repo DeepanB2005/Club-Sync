34402124fdf0e80e0ed068bf3c77bc862715dbf5
DeepanB2005 <deepanb2005@gmail.com>
2025-12-27 14:40:46 +0530

    cache

--- bc/server.js
+++ bc/server.js
@@ -45,7 +45,7 @@ app.use(cors({
     return callback(null, false);
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 }));
 
 app.use(express.json({ limit: '10mb' }));
